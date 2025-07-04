27c0aba1147f54d67913e6aa16a8644e3b682319
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-07-04 13:16:06 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,15 +6,15 @@ const app = express();
 const port = 8080;
 
 app.use(express.static(path.join(__dirname, '../src')));
-app.use('/images', express.static(path.join(__dirname, '../images')));
-app.use('/cases', express.static(path.join(__dirname, '../cases')));
+app.use('/images', express.static(path.join(__dirname, '../src/images'))); // Updated to src/images
+app.use('/cases', express.static(path.join(__dirname, '../src/cases')));  // Updated to src/cases
 
 app.get('/api/cases', async (req, res) => {
   try {
-    const files = (await fs.readdir(path.join(__dirname, '../cases')))
+    const files = (await fs.readdir(path.join(__dirname, '../src/cases'))) // Updated path
       .filter(file => file.endsWith('.json'));
     const cases = await Promise.all(files.map(async file => {
-      const data = await fs.readFile(path.join(__dirname, '../cases', file), 'utf8');
+      const data = await fs.readFile(path.join(__dirname, '../src/cases', file), 'utf8'); // Updated path
       return { file, ...JSON.parse(data) };
     }));
     res.json(cases);
